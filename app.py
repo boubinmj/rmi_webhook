f77c36a59b6566e18c75b568b2b6613b9a7142a8
Matt B <mjb353@nyu.edu>
2025-08-17 07:35:06 -0400

    added session to maintain state

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, request, jsonify
+import time
 
 app = Flask(__name__)
+
+SESSIONS = {}
+
+def get_sid():
+    # Caller should send ?session_id=123
+    sid = request.args.get("session_id")
+    if not sid:
+        # fallback: generate one if not provided
+        sid = str(int(time.time() * 1000))
+    return sid
 
 ## Webhook Payload Test
 ## Only works with GoogleDialogflowCX Playbook Structure
@@ -20,30 +31,42 @@ def appointment():
 
 @app.route('/firstName', methods=['GET'])
 def firstName():
-    # Get the parameter from query string (?input=...)
-    user_input = request.args.get('input')
-    
-    if not user_input:
+    sid = get_sid()
+    # (?input=...)
+    first_name = request.args.get('input')
+
+    # bad input    
+    if not first_name:
         return jsonify({"error": "Missing first name"}), 400
     
-    return jsonify({"you_sent": user_input})
+    SESSIONS.setdefault(sid, {})["first_name"] = first_name
+    return jsonify({"session_id": sid, "you_sent": first_name})
 
 @app.route('/lastName', methods=['GET'])
 def lastName():
+    sid = get_sid()
     # Get the parameter from query string (?input=...)
     user_input = request.args.get('input')
     
     if not user_input:
         return jsonify({"error": "Missing Last Name"}), 400
     
-    return jsonify({"you_sent": user_input})
+    SESSIONS.setdefault(sid, {})["first_name"] = user_input
+    return jsonify({"session_id": sid, "you_sent": user_input})
 
 @app.route('/email', methods=['GET'])
 def email():
+    sid = get_sid()
     # Get the parameter from query string (?input=...)
     user_input = request.args.get('input')
     
     if not user_input:
         return jsonify({"error": "Missing email"}), 400
     
-    return jsonify({"you_sent": user_input})
+    SESSIONS.setdefault(sid, {})["first_name"] = user_input
+    return jsonify({"session_id": sid, "you_sent": user_input})
+
+@app.route('/test', methods=['GET'])
+def test():
+    sid = get_sid()
+    return jsonify({"session_id": sid, "you_sent": SESSIONS})
